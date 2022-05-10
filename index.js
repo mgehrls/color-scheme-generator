2465623068a79355b3338f680a07a26b47ff6924
Matt Gehrls <mattgehrls@gmail.com>
2022-05-10 10:40:04 -0400

    V1.2
    Layout updated to be more responsive and have a "color palette" already displayed. Javascript updated to remove unneeded global variables and updated color copy function to make it work more often.

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
 /* Global Variables */
-
-let colorID = ""
-let colorScheme = ""
-let colorSecHtml = ""
-let colorArray = []
 const colorSec = document.getElementById('color-sec')
-
 
 /* Button click grabs the color from the #color-picker, grabs the scheme
 from #color-schemes, then getColorArray()*/
 
 document.getElementById('color-btn').addEventListener("click", function(evt){
     evt.preventDefault()
-    colorID = document.getElementById('color-picker').value.replace('#', "")
-    colorScheme = document.getElementById('color-schemes').value
-    getColorArray()
+    const colorID = document.getElementById('color-picker').value.replace('#', "")
+    const colorScheme = document.getElementById('color-schemes').value
+    getColorArray(colorID, colorScheme)
 })
 
 /* Async function to get colors that meet the criteria specified. then 
 runs getColorHtml() + error handling */
 
-async function getColorArray(){
+async function getColorArray(id, scheme){
     try{
-    let res = await fetch(`https://www.thecolorapi.com/scheme?hex=${colorID}&mode=${colorScheme}&count=5`)
+    let res = await fetch(`https://www.thecolorapi.com/scheme?hex=${id}&mode=${scheme}&count=5`)
     let data = await res.json()
-    colorArray = data.colors
-    getColorHtml()} catch (err){
+    const colorArray = data.colors
+    renderColorHtml(colorArray)} catch (err){
         alert(err + "Please Try Again.")
     }}
 
-/* for loop */
-
-function getColorHtml(){
-    colorSecHtml = ""
+function renderColorHtml(colorArray){
+    let colorSecHtml = ""
     colorArray.forEach((color)=>{
         colorSecHtml += `
-        <div class='color' id='color' style="background-color:${color.hex.value}" onclick="grabColor(${color.hex.value})">
+        <div class='color' id='color' style="background-color:${color.hex.value}" onclick="grabColor('${color.hex.value}')">
             <div class='color--text'> 
-                <p class="color--name">${color.name.value}</p>
                 <p class="color--hex">${color.hex.value}</p>
-                <p class="color--rgb">${color.rgb.value}</p>
             </div>
          </div>`
     })
@@ -47,7 +37,22 @@ function getColorHtml(){
     console.log(colorSecHtml)
 }
 
-function grabColor(i){
-    navigator.clipboard.writeText(i);
-    alert("Copied '" + i + "' to clipboard.")
+function grabColor(i) {
+    navigator.clipboard.writeText(i)
+        .catch(err => {
+            // Here, we set <textarea> element to be appended to the document. 
+            const area = document.createElement('textarea')
+            document.body.appendChild(area)
+            // Then, we set its value to the string you want to copy to the clipboard. (i in this case)
+            area.value = i
+            // Then, we use area.select() to select the contents of the <textarea> element.
+            area.select()
+            // After this, we use Document.execCommand('copy') to copy the contents of the <textarea> to the clipboard.
+            document.execCommand('copy')
+            // Finally, we remove the <textarea> element from the document since it's no longer needed.
+            document.body.removeChild(area)
+        })
+        .finally(() => {
+            alert("Copied '" + i + "' to clipboard.")
+        })
 }
